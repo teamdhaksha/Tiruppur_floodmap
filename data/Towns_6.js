7db9dfd7557b63144ec08807c8c5f1876226db0d
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-02-03 12:14:42 +0530

    Add files via upload

---
+++ data/Towns_6.js
@@ -0,0 +1,14 @@
+var json_Towns_6 = {
+"type": "FeatureCollection",
+"name": "Towns_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Tiruppur" }, "geometry": { "type": "Point", "coordinates": [ 77.340653711913333, 11.119190855576999, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Udumalapet" }, "geometry": { "type": "Point", "coordinates": [ 77.250578935633655, 10.581058765546381, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Dharapuram" }, "geometry": { "type": "Point", "coordinates": [ 77.523870830723624, 10.736477075570489, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Vellakoil" }, "geometry": { "type": "Point", "coordinates": [ 77.705510403027432, 10.94870932793641, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Kangayam" }, "geometry": { "type": "Point", "coordinates": [ 77.55642768611466, 11.00796766456047, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Avinashi" }, "geometry": { "type": "Point", "coordinates": [ 77.266476552987001, 11.193268769859699, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Palladam" }, "geometry": { "type": "Point", "coordinates": [ 77.282208800801413, 10.99384120961334, -999999.0 ] } }
+]
+}
